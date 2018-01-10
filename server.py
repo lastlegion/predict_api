de3099310cadb51e1a7448b31f72c889a73fd099
Ganesh Iyer <lastlegion@gmail.com>
2018-01-10 15:43:09 +0000

    added some documentation

--- server.py
+++ server.py
@@ -9,20 +9,27 @@ def hello():
 	return "hello world"
 
 
-@app.route("/predict", methods=['GET', 'POST'])
+
+''' POST API for prediction '''
+@app.route("/predict", methods=['POST'])
 def predict():
 	if 'file' not in request.files:
 		print('No CSV file found')
-		return "No file part"
-	f = request.files['file']
-	f.save(secure_filename(f.filename))
+		return "No file part" 
+	f = request.files['file'] #fetch file in the HTTP request
+	f.save(secure_filename(f.filename)) # save file to disk
+	
+	''' Your tensorflow or black box code goes here'''
 	hello=tf.constant('Hello world')
 	sess = tf.Session()
 
 	a = tf.constant(4)
 	b = tf.constant(5)
 	add = tf.add(a,b)
-	return sess.run(hello)
+	''' End tensorflow code '''
+	
+
+	return sess.run(hello) # the response of POST goes here, it should return predicted values here.
 
 if __name__ == '__main__':
 	app.run(debug=True)
